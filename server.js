b88a8173750757823865f3c29f9cd33697ab918b
Roy Jimenez <royjimval@gmail.com>
2023-10-24 08:28:02 -0700

    Update server.js

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.post(`/posts`, makeCallback(addPost))
 // app.patch(`${apiRoot}/comments/:id`, makeCallback(patchComment))
 // app.patch(`${apiRoot}/comments`, makeCallback(patchComment))
 // app.get(`${apiRoot}/comments`, makeCallback(getComments))
+//Test codacy
 app.use(makeCallback(notFound))
 
 
