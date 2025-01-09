a1c2beba01c33974a92db8a042835f266aeccf31
Roy Jimenez <royjimval@gmail.com>
2025-01-09 10:31:48 -0800

    FEAT 8

--- server.js
+++ server.js
@@ -20,8 +20,8 @@ app.post(`/posts`, makeCallback(addPost))
 // app.get(`${apiRoot}/comments`, makeCallback(getComments))  
 app.use(makeCallback(notFound))
 
-// DEPLOY 7 
-// MORE COMMENTS
+// DEPLOY 8
+// MORE COMMENTS 8
 
 // listen for requests
 app.listen(3000, () => {
